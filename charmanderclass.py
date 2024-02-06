4d4deebce7cdf84795ff9c029a066e1007f81314
damianslavenburg <112872019+damianslavenburg@users.noreply.github.com>
2024-02-06 10:18:07 +0100

    made the charmander class

--- charmanderclass.py
+++ charmanderclass.py
@@ -1,1 +1,9 @@
 import pokemon_battle_simulator
+class Charmander:
+    def __init__(self, nickname, fire, water):
+        self.nickname = nickname
+        self.fire = fire
+        self.water = water
+
+    def battle_cry(self):
+        print(self.nickname + " says: Charmander!")
